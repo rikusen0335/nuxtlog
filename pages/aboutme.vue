099f6a9a55f16490ede05a4b32ede0088adfb245
rikusen0335 <rikusen0335@gmail.com>
2019-06-30 10:20:22 +0900

    add: aboutme page

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="container">
-    <p>unti buri</p>
+    <div>
+      <h1 class="title m-5">Who am I?</h1>
+      <div>
+        <div class="card">
+          <div class="card-body text-center">
+            <p class="mt-3">
+              <img
+                src="/pathfinder.jpg"
+                alt="card image">
+            </p>
+            <h2 class="card-title">rikusen0335</h2>
+            <p class="card-text">Hello, I'm Japanese Hiyoko Engineer. 18y/o.</p>
+            <p class="card-text mb-5">I can speak Japanese as native, and also speak English as noob but can do conversation a little.</p>
+            <h4 class="card-title">Skills</h4>
+            <p class="card-text mb-5">HTML, CSS, Python, Elixir, JavaScript, TypeScript, VueJS, NuxtJS, AngularJS and English.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.card {
+  width: 500px;
+  border: 1px solid #f2f8f9;
+
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+}
+</style>
