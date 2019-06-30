a95759d2ac771c197024dd01bcb2494ba89b047f
rikusen0335 <rikusen0335@gmail.com>
2019-06-30 10:23:46 +0900

    nandakke

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-vue-dark">
-      <n-link to="/" class="navbar-brand">Navbar</n-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-vue-dark sticky-top">
+      <n-link to="/" class="navbar-brand">でぶみあろぐ</n-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+        <ul class="navbar-nav">
+          <li class="nav-item">
             <n-link to="/" class="nav-link">Home <span class="sr-only">(current)</span></n-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item ml-auto">
             <n-link to="/aboutme" class="nav-link">About Me</n-link>
           </li>
         </ul>
